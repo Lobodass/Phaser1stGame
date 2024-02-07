bc15f410f03fe31a560c986dc2fce43f1096ccbd
Lobodass <158287475+Lobodass@users.noreply.github.com>
2024-02-07 12:58:39 +0200

    Додали фон

--- game.js
+++ game.js
@@ -13,10 +13,19 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('чувак',
+        'assets/dude.png',
+        {frameWidth: 32, frameHeight: 48}
+    );
 }
 
 function create ()
 {
+    this.add.image(400, 300, 'sky');
 }
 
 function update ()
